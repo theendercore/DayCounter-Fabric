2dc816489ff7666733ff69b41ae30729cf9cdf76
theendercore <theendercore@gmail.com>
2023-04-28 14:58:29 +0300

    functionally complete

--- src/main/kotlin/com/theendercore/DayCounter.kt
+++ src/main/kotlin/com/theendercore/DayCounter.kt
@@ -3,29 +3,25 @@ package com.theendercore
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback
 import net.minecraft.client.MinecraftClient
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
+import net.minecraft.client.util.math.MatrixStack
+import net.minecraft.text.Text
 
 class DayCounter : ModInitializer {
-
     override fun onInitialize() {
-        LOGGER.info("hi $MODID")
-        HudRenderCallback.EVENT.register { matrixStack, _ ->
+        HudRenderCallback.EVENT.register { matrixStack: MatrixStack, _: Float ->
             val client = MinecraftClient.getInstance()
             client.textRenderer.draw(
                 matrixStack,
-                "hello",
-                client.window.width / 2 + 100f,
-                client.window.height / 2 - 20f,
+                Text.of(String.format("%d", client.world?.timeOfDay?.div(24000) ?: 0)),
+                (client.window.scaledWidth / 2f) + 95f,
+                client.window.scaledHeight - 15f,
                 0xffffff
             )
-
         }
     }
 
-    companion object {
-        const val MODID: String = "day_counter"
-        val LOGGER: Logger = LoggerFactory.getLogger(MODID)
-//        fun id(path: String): Identifier = Identifier(MODID, path)
-    }
+    /*    companion object {
+            const val MODID: String = "day_counter"
+            val LOGGER: Logger = LoggerFactory.getLogger(MODID)
+    }*/
 }
