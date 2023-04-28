075373722b304a23311bd2cf5b4ad56e9a57566d
theendercore <theendercore@gmail.com>
2023-04-28 16:35:26 +0300

    Added Shadows & Fixed spectator rendering

--- src/main/kotlin/com/theendercore/DayCounter.kt
+++ src/main/kotlin/com/theendercore/DayCounter.kt
@@ -4,24 +4,18 @@ import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback
 import net.minecraft.client.MinecraftClient
 import net.minecraft.client.util.math.MatrixStack
-import net.minecraft.text.Text
 
 class DayCounter : ModInitializer {
     override fun onInitialize() {
         HudRenderCallback.EVENT.register { matrixStack: MatrixStack, _: Float ->
             val client = MinecraftClient.getInstance()
-            client.textRenderer.draw(
+            if (!(client.player?.isSpectator())!!) client.textRenderer.drawWithShadow(
                 matrixStack,
-                Text.of(String.format("%d", client.world?.timeOfDay?.div(24000) ?: 0)),
+                String.format("Day %d", client.world?.timeOfDay?.div(24000) ?: 0),
                 (client.window.scaledWidth / 2f) + 95f,
                 client.window.scaledHeight - 15f,
                 0xffffff
             )
         }
     }
-
-    /*    companion object {
-            const val MODID: String = "day_counter"
-            val LOGGER: Logger = LoggerFactory.getLogger(MODID)
-    }*/
 }
